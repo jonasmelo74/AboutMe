513a7386d98b2f6f792e272d7c61f7361d9c6ce6
Jonas Melo <jonasmelodapaz74@gmail.com>
2023-01-18 21:57:04 -0300

    resolvendo bug

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -24,3 +24,16 @@ function setActiveStyle(color)
     }
   })
 }
+
+//light and dark mode
+const dayNight = document.querySelector(".day-night");
+window.addEventListener("load", () => {
+  if(document.body.classList.contains("dark"))
+  {
+    dayNight.querySelector("i").classList.add("fa-sun");
+  }
+  else
+  {
+    dayNight.querySelector("i").classList.add("fa-moon")
+  }
+});
