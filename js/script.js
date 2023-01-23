ee9d53c314f2bf8a475646127ef58ef9ce04824b
Jonas Melo <jonasmelodapaz74@gmail.com>
2023-01-23 19:58:24 -0300

    fazendo o responsivo

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var typed = new Typed(".typing",{
+  strings:[ "Web Designer", "Web Developer", "Graphic Designer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
+})
+
+const nav = document.querySelector(".nav"),
+      navList = nav.querySelectorAll("li"),
+      totalNavList = navList.length;
+      for(let i=0; i<totalNavList; i++)
+      {
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click", function()
+        {
+          for(let j = 0; j < totalNavList.length; j++)
+          {
+            navList[j].querySelector("a").classList.remove("active"); 
+          }
+          this.classList.add("active");
+          showSection(this)
+        })
+      }
+      function showSection(element)
+      {
+        const href=element.getAttribute("href").split("#")
+        target = href[1];
+        
+      }
--- js/style-switcher.js
+++ js/style-switcher.js
@@ -27,6 +27,11 @@ function setActiveStyle(color)
 
 //light and dark mode
 const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");  
+})
 window.addEventListener("load", () => {
   if(document.body.classList.contains("dark"))
   {
