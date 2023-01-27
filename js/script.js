574a83cfbb9254f16765b5f6a43f0a95b251d981
Jonas Melo <jonasmelodapaz74@gmail.com>
2023-01-27 00:01:06 -0300

    Adicionando botao de responsivo

--- js/script.js
+++ js/script.js
@@ -15,22 +15,38 @@ const nav = document.querySelector(".nav"),
         const a = navList[i].querySelector("a");
         a.addEventListener("click", function()
         {
+          for(let i = 0; i < totalSection; i++)
+        {
+          allSection[i].classList.remove("back-section");
+        }
           for(let j = 0; j < totalNavList; j++)
           {
+            if(navList[j].querySelector("a").classList.contains("active"))
+            {
+              allSection[j].classList.add("back-section");
+            }
             navList[j].querySelector("a").classList.remove("active"); 
           }
           this.classList.add("active");
           showSection(this)
-          console.log('Teste')
         })
       }
       function showSection(element)
       {
-        for(let i = 0; i < totalSection, i++)
+        for(let i = 0; i < totalSection; i++)
         {
           allSection[i].classList.remove("active");
         }
         const target = element.getAttribute("href").split("#")[1]; 
         document.querySelector("#" + target).classList.add("active")
-        console.log(target)
       }
+      const navTogglerBtn = document.querySelector(".nav-toggler"),
+        aside = document.querySelector(".aside");
+        navTogglerBtn.addEventListener("click", () => 
+        {
+          asideSectionTogglerBtn();
+        })
+        function asideSectionTogglerBtn()
+        {
+          aside.classList.toggle("open");
+        }
